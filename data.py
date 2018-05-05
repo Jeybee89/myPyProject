c7dcee6486bec708d6d6467e092bdef76bc7a3b0
Nick <nelson.nikolas@gmail.com>
2018-05-05 19:57:40 +0100

    Initial commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquam, laudantium libero neque nesciunt odio officiis repudiandae rerum soluta ut veritatis voluptatem voluptatum.',
+            'author':'Nick',
+            'create_date':'05-05-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquam, laudantium libero neque nesciunt odio officiis repudiandae rerum soluta ut veritatis voluptatem voluptatum.',
+            'author': 'Nick Nel',
+            'create_date': '05-05-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquam, laudantium libero neque nesciunt odio officiis repudiandae rerum soluta ut veritatis voluptatem voluptatum.',
+            'author': 'Nick',
+            'create_date': '05-05-2018'
+        }
+    ]
+    return articles
